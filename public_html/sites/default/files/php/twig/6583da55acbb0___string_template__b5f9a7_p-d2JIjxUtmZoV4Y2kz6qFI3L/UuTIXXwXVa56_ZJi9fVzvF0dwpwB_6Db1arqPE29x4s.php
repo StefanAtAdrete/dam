cdd05df6ab24f734aadd2d53754a9c9cfa5ca155
Stefan Carlsson <stefan@adrete.se>
2023-12-21 08:03:36 +0100

    Drupal 10 update

---
+++ public_html/sites/default/files/php/twig/6583da55acbb0___string_template__b5f9a7_p-d2JIjxUtmZoV4Y2kz6qFI3L/UuTIXXwXVa56_ZJi9fVzvF0dwpwB_6Db1arqPE29x4s.php
@@ -0,0 +1,116 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* __string_template__b5f9a7cb8674db8476b9666c60d2046d */
+class __TwigTemplate_566b7fb6c021d998a3c1bb312b96d0f3 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<a href=\"";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["view_taxonomy_term"] ?? null), 1, $this->source), "html", null, true);
+        echo "\" class=\"text-decoration-none text-secondary\">
+<div class=\"card border border-0 shadow p-0 bg-body-tertiary rounded\">
+  <div class=\"card-img-top\">";
+        // line 3
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["field_taxonomy_media"] ?? null), 3, $this->source), "html", null, true);
+        echo "</div>
+  <div class=\"card-body\">
+    <h5 class=\"card-title\"><a href=\"";
+        // line 5
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["view_taxonomy_term"] ?? null), 5, $this->source), "html", null, true);
+        echo "\" class=\"text-decoration-none text-secondary\">";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["name"] ?? null), 5, $this->source), "html", null, true);
+        echo "</h5>
+  </div>
+</div>
+</a>";
+        $this->env->getExtension('\Drupal\Core\Template\TwigExtension')
+            ->checkDeprecations($context, ["view_taxonomy_term", "field_taxonomy_media", "name"]);    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getTemplateName()
+    {
+        return "__string_template__b5f9a7cb8674db8476b9666c60d2046d";
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getDebugInfo()
+    {
+        return array (  50 => 5,  45 => 3,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "__string_template__b5f9a7cb8674db8476b9666c60d2046d", "");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array();
+        static $filters = array("escape" => 1);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                [],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
--- public_html/sites/default/settings.php
+++ public_html/sites/default/settings.php
@@ -725,7 +725,6 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  * @see https://www.drupal.org/docs/installing-drupal/trusted-host-settings
  */
 
-
 /**
  * The default list of directories that will be ignored by Drupal's file API.
  *
